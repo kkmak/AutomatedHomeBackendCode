44eb56dc75941f75e600c5ff4d5c7b2d18218c12
But Marius <marius.but25.com>
2023-07-24 20:34:58 +0300

    First commit

---
+++ src/main/java/org/fasttrackit/SimulatedAutomatedHome/SimulatedAutomatedHomeApplication.java
@@ -0,0 +1,13 @@
+package org.fasttrackit.SimulatedAutomatedHome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimulatedAutomatedHomeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimulatedAutomatedHomeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/fasttrackit/SimulatedAutomatedHome/SimulatedAutomatedHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fasttrackit.SimulatedAutomatedHome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimulatedAutomatedHomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
